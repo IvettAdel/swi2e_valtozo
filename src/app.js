5d337be79a0623d484a13ac70baea4b095c0ff43
Zsiga Gergely <zsi-ga>
2023-02-09 19:52:35 +0100

    gyak vége

--- src/app.js
+++ src/app.js
@@ -1,22 +1,61 @@
-var szam = "35";
-var szam2 = '27';
+/*const num1 = 37;
+console.log(num1);
+if (num1 == 37) {
+    var a = 35;
+    let d = 47
+    console.log(d)
 
-console.log(Number(szam) + Number(szam2));
+}
+var b = 37;
 
-var esik = true;
-if (esik) {
-    console.log('esik....')
+const num1Elem = document.querySelector('#num1');
+const getButton =document.querySelector('#getButton');
 
-    function calcArea(base, height) {
-        return base * height /2
+
+getButton.addEventListener('click', () => {
+    var szam = 3;
+    szam = num1Elem.value;
+    num1Elem.value = ' ';
+    kiir (szam);
+    vizsgalat(szam);
+});
+
+function vizsgalat(input) {
+    let egyezik = input.match(/[1-3]/);
+    if egyezik = input.match(/[0-9]/);
+        console.log(egyezik);
+    
+    }else{
+        console.log('Nem jó')
+        }
+ 
+
+function kiir () {
+
+    if (szam == 1) {
+    console.log('Egy');
+
+    }else if (szam == 3) {
+    console.log('három');
+    
+    }else{
+    console.log('Ismeretlen érték')
     }
 }
-console.log(typeof calcArea)
 
-var pista = {
-    nev:'pisti',
-    kor: 28
-};
-console.log(typeof pista)
-var szamok = [];
-console.log(typeof szamok);
+console.log(window.screen.width)
+console.log(window.screen.height)
+console.error('uuuuuuuuuuuuu')*/
+
+const doboz1 = document.querySelector('#doboz1');
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', () => {
+    doboz1.style.backgroundColor = 'blue';
+    doboz1.style.animation = 'valami1 10s';
+
+}); 
+var szamokTomb = [35, 42, 27, 55, 72];
+szamokTomb.forEach( (szam) => {
+    console.log(szam);
+});
