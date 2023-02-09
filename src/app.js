95c04ca52c41fc5033896157f91e72fce6352112
IvettAdel <116666856+IvettAdel@users.noreply.github.com>
2023-02-09 17:20:16 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+var szam = "35";
+var szam2 = '27';
+
+console.log(Number(szam) + Number(szam2));
+
+var esik = true;
+if (esik) {
+    console.log('esik....')
+
+    function calcArea(base, height) {
+        return base * height /2
+    }
+}
+console.log(typeof calcArea)
+
+var pista = {
+    nev:'pisti',
+    kor: 28
+};
+console.log(typeof pista)
+var szamok = [];
+console.log(typeof szamok);
